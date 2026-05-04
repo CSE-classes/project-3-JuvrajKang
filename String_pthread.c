764162210ac28ce66778c002ab07049d9344c0a2
Juvraj Kang <jkk9933@mavs.uta.edu>
2026-05-04 04:04:33 +0000

    PROJECT 3

--- String_pthread.c
+++ String_pthread.c
@@ -1,83 +1,147 @@
-
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define NUM_THREADS	4
+#define NUM_THREADS     4
 #define MAX 1024
 
 void *sub_string(void *);
 int readf(FILE *fp);
-int total=0;
-int nlocal,n1,n2;
-char *s1,*s2;
+
+int total = 0;
+int nlocal, n1, n2;
+char *s1, *s2;
 FILE *fp;
 pthread_mutex_t total_lock;
 
 int main(int argc, char *argv[])
 {
-	int i,rc;
-	pthread_t threads[NUM_THREADS];
+        int i, rc;
+        pthread_t threads[NUM_THREADS];
 
-	pthread_mutex_init(&total_lock,NULL);
-	readf(fp);
-	for(i=0;i<NUM_THREADS;i++){
-		rc=pthread_create(&threads[i],NULL,sub_string,(void *)i);
-		if (rc){
-			printf("ERROR: return error from pthread_create() is %d\n", rc);
-			exit(-1);
-		}
-	}
+        pthread_mutex_init(&total_lock, NULL);
 
-	for(i=0; i<NUM_THREADS; i++){
-		rc = pthread_join(threads[i], NULL);
-		if (rc){
-			printf("ERROR: return error from pthread_join() is %d\n", rc);
-			exit(-1);
-		}
-	}
-	printf("the occurences of s2 in s1 is %d\n",total);
-	pthread_exit(0);
+        if (readf(fp) != 0)
+        {
+                exit(-1);
+        }
+
+        for (i = 0; i < NUM_THREADS; i++)
+        {
+                rc = pthread_create(&threads[i], NULL, sub_string, (void *)(long)i);
+                if (rc)
+                {
+                        printf("ERROR: return error from pthread_create() is %d\n", rc);
+                        exit(-1);
+                }
+        }
+
+        for (i = 0; i < NUM_THREADS; i++)
+        {
+                rc = pthread_join(threads[i], NULL);
+                if (rc)
+                {
+                        printf("ERROR: return error from pthread_join() is %d\n", rc);
+                        exit(-1);
+                }
+        }
+
+        printf("the occurences of s2 in s1 is %d\n", total);
+
+        pthread_mutex_destroy(&total_lock);
+        free(s1);
+        free(s2);
+
+        return 0;
 }
-
-
 
 int readf(FILE *fp)
 {
-	if((fp=fopen("strings.txt", "r"))==NULL){
-		printf("ERROR: can't open string.txt!\n");
-		return 0;
-	}
-	s1=(char *)malloc(sizeof(char)*MAX);
-	if(s1==NULL){
-		printf("ERROR: Out of memory!\n");
-		return -1;
-	}
-	s2=(char *)malloc(sizeof(char)*MAX);
-	if(s1==NULL){
-		printf("ERROR: Out of memory\n");
-		return -1;
-	}
-	/*read s1 s2 from the file*/
-	s1=fgets(s1, MAX, fp);
-	s2=fgets(s2, MAX, fp);
-	n1=strlen(s1);  /*length of s1*/
-	n2=strlen(s2)-1; /*length of s2*/
-	nlocal=n1/NUM_THREADS;  /*data length held by process*/
-	if(s1==NULL || s2==NULL ||n1<n2)  /*when error exit*/
-		return -1;
+        if ((fp = fopen("strings.txt", "r")) == NULL)
+        {
+                printf("ERROR: can't open strings.txt!\n");
+                return -1;
+        }
+
+        s1 = (char *)malloc(sizeof(char) * MAX);
+        if (s1 == NULL)
+        {
+                printf("ERROR: Out of memory!\n");
+                fclose(fp);
+                return -1;
+        }
+
+        s2 = (char *)malloc(sizeof(char) * MAX);
+        if (s2 == NULL)
+        {
+                printf("ERROR: Out of memory\n");
+                fclose(fp);
+                free(s1);
+                return -1;
+        }
+
+        s1 = fgets(s1, MAX, fp);
+        s2 = fgets(s2, MAX, fp);
+
+        fclose(fp);
+
+        if (s1 == NULL || s2 == NULL)
+        {
+                return -1;
+        }
+
+        n1 = strlen(s1);
+        n2 = strlen(s2) - 1;
+        nlocal = n1 / NUM_THREADS;
+
+        if (n1 < n2)
+        {
+                return -1;
+        }
+
+        return 0;
 }
 
-void *sub_string(void *threadid) 	/*each process searches in the string with the step of nprocs until it reach or beyond*/ 
-	/*the (n1-n2)th char which is the last possible beginning of the substring*/
+void *sub_string(void *threadid)
 {
+        long tid = (long)threadid;
+        int start = tid * nlocal;
+        int end = start + nlocal - 1;
+        int i, j, k;
+        int count;
+        int local_total = 0;
 
+        if (end > n1 - n2)
+        {
+                end = n1 - n2;
+        }
+
+        for (i = start; i <= end; i++)
+        {
+                count = 0;
+
+                for (j = i, k = 0; k < n2; j++, k++)
+                {
+                        if (*(s1 + j) != *(s2 + k))
+                        {
+                                break;
+                        }
+                        else
+                        {
+                                count++;
+                        }
+                }
+
+                if (count == n2)
+                {
+                        local_total++;
+                }
+        }
+
+        pthread_mutex_lock(&total_lock);
+        total += local_total;
+        pthread_mutex_unlock(&total_lock);
+
+        pthread_exit(NULL);
 }
-
-
-
-
-
-
-
